77bc8275746a49d20930180316243de3a4740212
AbhayGoyal001 <abhaygoyalscience@gmail.com>
2025-01-20 11:31:58 +0530

    Files Added

---
+++ src/Assignment1/Problem10.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the distance in kilometers: ");
+        double kilometers = scanner.nextDouble();
+
+        double miles = kilometers * 0.621371;
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+
+    }
+}
